9974af0c25810206f022f2a8e388fa1ddfc0e965
XYZ <XYZ>
2016-03-07 16:02:22 +0100

    client changes

---
+++ GamerClient/src/gamer/UdpClient.java
@@ -0,0 +1,53 @@
+package gamer;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.util.Arrays;
+import java.util.List;
+
+public class UdpClient {
+	public static final String REMOTE = "10.254.22.19";
+	
+	public static void main(String[] args) throws Exception {
+		UdpClient client = new UdpClient();
+		
+		client.map();
+	}
+	
+	public void map() throws Exception {
+		sendTxtAndShowOnOutput(Arrays.asList(Messages.MAP));
+	}
+	
+	private void sendTxtAndShowOnOutput(List<String> args) throws Exception {
+
+		DatagramSocket socket = new DatagramSocket();
+
+		String msg = concatMsg(args);
+		
+		// send request
+		byte[] buf = msg.getBytes();
+		InetAddress address = InetAddress.getByName(REMOTE);
+		DatagramPacket packet = new DatagramPacket(buf, buf.length, address, 4445);
+		socket.send(packet);
+		
+		byte[] recvBuf = new byte[1024];
+		DatagramPacket received = new DatagramPacket(recvBuf, recvBuf.length);
+		socket.receive(received);
+		socket.close();
+		
+		System.out.println("RECEIVED: "+new String(recvBuf,0,received.getLength()));
+
+	}
+
+	private String concatMsg(List<String> args) {
+		String msg = new String();
+		for (String each : args) {
+			msg += each + ";";
+		}
+		
+		return msg.substring(0, msg.length()-1);
+		
+	}
+}
