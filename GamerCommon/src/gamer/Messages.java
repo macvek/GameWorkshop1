00c9423bba832c8b14ad353936f938c470436757
XYZ <XYZ>
2016-03-07 15:53:32 +0100

    wiadomosci

--- GamerCommon/src/gamer/Messages.java
+++ GamerCommon/src/gamer/Messages.java
@@ -5,4 +5,9 @@ public class Messages {
 	public static final String PONG="PONG";
 	public static final String STOP="STOP";
 	public static final String DONTKNOW="DONTKNOW";
+	public static final String MAP="MAP";
+	public static final String MOVE="MOVE";
+	public static final String CREATE="CREATE";
+	public static final String LIST="LIST";
+	public static final String APPLE="APPLE";
 }
